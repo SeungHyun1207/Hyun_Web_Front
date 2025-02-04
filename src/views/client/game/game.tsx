c424d49744ac3ea3f59c9845088073efc0643f11
Seunghyun Kang <seunghyun_1207@naver.com>
2025-02-04 16:24:01 +0900

    perf : 반지,펜던트 일부 수정
    perf : 반지,펜던트 일부 수정

--- src/views/client/game/game.tsx
+++ src/views/client/game/game.tsx
@@ -13,19 +13,24 @@ import {
 } from './../../../apis/hooks/client/game/stores/useGameStore';
 
 const equipmentOrder = [
-  ['반지', '', '모자', '', '엠블렘'],
-  ['반지', '펜던트', '얼굴장식', '', '뱃지'],
-  ['반지', '펜던트', '눈장식', '귀고리', '훈장'],
-  ['반지', '무기', '상의', '어깨장식', '보조무기'],
+  ['반지1', '', '모자', '', '엠블렘'],
+  ['반지2', '펜던트', '얼굴장식', '', '뱃지'],
+  ['반지3', '펜던트2', '눈장식', '귀고리', '훈장'],
+  ['반지4', '무기', '상의', '어깨장식', '보조무기'],
   ['포켓 아이템', '벨트', '하의', '장갑', '망토'],
   ['', '', '신발', '안드로이드', '기계 심장'],
 ];
 
 const equipmentClassName = new Map([
   ['반지', 'ringInfo'],
+  ['반지1', 'ringInfo'],
+  ['반지2', 'ringInfo'],
+  ['반지3', 'ringInfo'],
+  ['반지4', 'ringInfo'],
   ['모자', 'capInfo'],
   ['엠블렘', 'emblemInfo'],
   ['펜던트', 'pendantInfo'],
+  ['펜던트2', 'pendantInfo'],
   ['얼굴장식', 'foreheadInfo'],
   ['뱃지', 'badgeInfo'],
   ['눈장식', 'eyeInfo'],
@@ -35,7 +40,7 @@ const equipmentClassName = new Map([
   ['상의', 'clothesInfo'],
   ['어깨장식', 'shoulderInfo'],
   ['보조무기', 'subWeaponInfo'],
-  ['포켓 아이템', 'pocketInfo'],
+  ['포켓 아이템', 'poketInfo'],
   ['벨트', 'beltInfo'],
   ['하의', 'pantsInfo'],
   ['장갑', 'glovesInfo'],
@@ -76,9 +81,7 @@ const Game = () => {
                 const className =
                   equipmentClassName.get(slotName) || 'defaultClass';
                 const item = characterItemEquipments.find(
-                  (item) =>
-                    item.item_equipment_slot === slotName ||
-                    item.item_equipment_part === slotName
+                  (item) => item.item_equipment_slot === slotName
                 );
                 return (
                   <div key={colIndex} className={`${className} equipment-slot`}>
