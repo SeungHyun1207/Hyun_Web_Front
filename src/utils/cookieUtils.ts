885bfbf36267a8c5b96e44f57fb45679fa9e89c1
Seunghyun Kang <seunghyun_1207@naver.com>
2025-02-12 09:17:27 +0900

    feat : 쿠키라이브러리 추가
    feat : 쿠키라이브러리 추가

---
+++ src/utils/cookieUtils.ts
@@ -0,0 +1,25 @@
+/**
+ *  작성일 : 2025-02-12
+ *  작성자 : SH.K
+ *  내용 : ReactCookie
+ */
+import { Cookies } from 'react-cookie';
+import { CookieSetOptions } from 'universal-cookie';
+
+const cookies = new Cookies();
+
+export const setCookie = (
+  name: string,
+  value: string,
+  options?: CookieSetOptions
+) => {
+  return cookies.set(name, value, { ...options, path: '/' });
+};
+
+export const getCookie = (name: string) => {
+  return cookies.get(name);
+};
+
+export const removeCookie = (name: string) => {
+  return cookies.remove(name);
+};
